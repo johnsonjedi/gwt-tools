91c19b2fc80034c22e04ed5d380bdcbda991e961
Johnson Hsu <johnson.jedi@gmail.com>
2015-07-27 12:17:30 +0800

    add license

--- src/main/java/idv/john/gwt/tools/annotation/GwtEventRegistration.java
+++ src/main/java/idv/john/gwt/tools/annotation/GwtEventRegistration.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2015 Johnson Hsu 
+ * 
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *  http://www.gnu.org/licenses/lgpl-3.0.txt 
+ *  
+ *  Unless required by applicable law or agreed to in writing, software 
+ *  distributed under the License is distributed on an "AS IS" BASIS, 
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ *  See the License for the specific language governing permissions and 
+ *  limitations under the License. 
  */
 package idv.john.gwt.tools.annotation;
 
@@ -18,8 +28,16 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.CLASS)
 public @interface GwtEventRegistration {
     
+    /** 
+     * (Required) 
+     * Name of GWT Event. 
+     */
     String name();
     
+    /** 
+     * (Required) 
+     * An array of <code>EventAction</code> annotations. 
+     */
     EventAction [] actions();
     
 }
